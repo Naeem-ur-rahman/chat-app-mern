25f0d54b48b9998cb5da8a4fb3aef6fb8910c4c9
Naeem-ur-rahman <naeemur1001@gmail.com>
2024-08-16 23:39:44 +0500

    Check the userInfo for all pages and profilesetup check in chat page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,44 @@
-import React from 'react';
+import React, { Children } from 'react';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import Auth from './pages/auth';
 import Chat from './pages/chat';
 import Profile from './pages/profile';
+import { useAppStore } from './store';
+
+const ChatRoute = ({ childern }) => {
+  const { userInfo } = useAppStore();
+  const isAutenticated = !!userInfo;
+  return isAutenticated ? <Chat /> : <Navigate to='/auth' />;
+}
+
+const ProfileRoute = ({ childern }) => {
+  const { userInfo } = useAppStore();
+  const isAutenticated = !!userInfo;
+  return isAutenticated ? <Profile /> : <Navigate to='/auth' />;
+}
+
+const AuthRoute = ({ childern }) => {
+  const { userInfo } = useAppStore();
+  const isAutenticated = !!userInfo;
+  return isAutenticated ? <Navigate to='/chat' /> : <Auth />;
+}
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/auth' element={<Auth />} />
-        <Route path='/chat' element={<Chat />} />
-        <Route path='/profile' element={<Profile />} />
-        <Route path='*' element={<Navigate to="/auth" />} />
-      </Routes>
-    </BrowserRouter>
+        <Route path='/auth' element={
+          <AuthRoute />
+        } />
+        < Route path='/chat' element={
+          <ChatRoute />
+        } />
+        < Route path='/profile' element={
+          <ProfileRoute />
+        } />
+        < Route path='*' element={< Navigate to="/auth" />} />
+      </Routes >
+    </BrowserRouter >
   );
 }
 
--- client/src/pages/chat/index.jsx
+++ client/src/pages/chat/index.jsx
@@ -1,4 +1,17 @@
+import { useAppStore } from "@/store";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "sonner";
+
 const Chat = () => {
+    const navigate = useNavigate();
+    const { userInfo } = useAppStore();
+    useEffect(() => {
+        if (!userInfo.profileStatus) {
+            toast("Please Setup Profile to continue.")
+            navigate('/profile')
+        }
+    }, [navigate, userInfo])
     return (
         <div>
             Chat
