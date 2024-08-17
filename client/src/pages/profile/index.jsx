5d841fed19b9121489b0bdc02dd3e42847127c29
Naeem-ur-rahman <naeemur1001@gmail.com>
2024-08-17 17:40:53 +0500

    Profile UI

--- client/src/pages/profile/index.jsx
+++ client/src/pages/profile/index.jsx
@@ -1,12 +1,108 @@
 import { useAppStore } from "@/store";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { IoArrowBack } from 'react-icons/io5'
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { colors, getColor } from "@/lib/utils";
+import { FaTrash, FaPlus } from "react-icons/fa"
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
 const Profile = () => {
-    const { userInfo } = useAppStore()
+    const navigate = useNavigate()
+    const { userInfo, setUserInfo } = useAppStore()
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [selectedColor, setSelectedColor] = useState(0);
+    const [image, setImage] = useState(null);
+    const [hovered, setHovered] = useState(false);
+
+    const saveChanges = () => { }
     return (
-        <div>
-            <div>Email {userInfo?.email}</div>
-            <div>Email {userInfo?.id}</div>
-            Profile
+        <div className="bg-[#1b1c24] h-[100vh] flex items-center justify-center flex-col gap-10">
+            <div className="flex flex-col gap-10 w-[80vw] md:w-max ">
+                <div className="gap-5 flex flex-col">
+                    <IoArrowBack className="text-4xl lg:text-6xl text-white/90 cursor-pointer" />
+                    <div className="grid grid-cols-2">
+                        <div className="h-full w-32 md:w-48 md:h-48 relative flex items-center justify-center"
+                            onMouseEnter={() => setHovered(true)}
+                            onMouseLeave={() => setHovered(false)}
+                        >
+                            <Avatar className="w-32 h-32 md:w-48 md:h-48">
+                                {
+                                    image ?
+                                        <AvatarImage src={image} alt="Profile" className="object-cover h-full w-full rounded-full bg-black" />
+                                        :
+                                        <div className={`w-32 h-32 md:w-48 md:h-48 text-5xl border-[1px] rounded-full flex items-center justify-center ${getColor(selectedColor)}`}>
+                                            {
+                                                firstName ?
+                                                    firstName.split('').shift()
+                                                    :
+                                                    userInfo.email.split('').shift().toUpperCase()
+                                            }
+                                        </div>
+                                }
+                            </Avatar>
+                            {
+                                hovered && (
+                                    <div className="absolute inset-0 flex items-center justify-center bg-black/50 cursor-pointer rounded-full ">
+                                        {image ? <FaTrash className="text-white cursor-pointer text-3xl" /> : <FaPlus className="text-white cursor-pointer text-3xl" />}
+                                    </div>
+                                )}
+                            {/* <input type="text" /> */}
+                        </div>
+                        <div className="flex min-w-32 md:min-w-64 flex-col gap-5 text-white items-center justify-center">
+                            <div className="w-full">
+                                <Input
+                                    placeholder="Email"
+                                    type="email"
+                                    disabled
+                                    value={userInfo.email}
+                                    className="rounded-lg p-4 bg-[#2c2e3b] border-none"
+                                />
+                            </div>
+                            <div className="w-full">
+                                <Input
+                                    placeholder="First Name"
+                                    type="text"
+                                    onChange={e => setFirstName(e.target.value)}
+                                    value={firstName}
+                                    className="rounded-lg p-4 bg-[#2c2e3b] border-none"
+                                />
+                            </div>
+                            <div className="w-full">
+                                <Input
+                                    placeholder="Last Name"
+                                    type="text"
+                                    onChange={e => setLastName(e.target.value)}
+                                    value={lastName}
+                                    className="rounded-lg p-4 bg-[#2c2e3b] border-none"
+                                />
+                            </div>
+                            <div className="w-full flex gap-5">
+                                {
+                                    colors.map((color, index) =>
+                                    (<div className={`${color} w-8 h-8 rounded-full cursor-pointer transition-all duration-300 
+                                    ${selectedColor === index ?
+                                            "outline outline-1 outline-white" : ""
+                                        }
+                                    `}
+                                        key={index}
+                                        onClick={() => (setSelectedColor(index))}
+                                    ></div>))
+                                }
+                            </div>
+                        </div>
+                    </div>
+                    <div className="w-full">
+                        <Button className="h-16 w-full bg-purple-700 hover:bg-purple-900 cursor-pointer transition-all duration-300"
+                            onClick={saveChanges}
+                        >
+                            Save Changes
+                        </Button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
