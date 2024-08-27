0244ff04d5b01b7e7c0708b16ded6e1edd30858c
Naeem-ur-rahman <naeemur1001@gmail.com>
2024-08-27 23:22:02 +0500

    get-contacts-for-dm added route

--- server/routes/contacts.js
+++ server/routes/contacts.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { verifyToken } from "../middlewares/authMiddleware.js";
-import { searchContacts } from "../controllers/contacts.js";
+import { getContactsForDMList, searchContacts } from "../controllers/contacts.js";
 
 const router = Router()
 
 router.post('/search', verifyToken, searchContacts);
+router.get('/get-contacts-for-dm', verifyToken, getContactsForDMList);
 
 export default router;
