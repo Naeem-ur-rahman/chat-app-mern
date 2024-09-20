a74302ad1f1f9a8c05050485fb991c63b37c438b
Naeem-ur-rahman <naeemur1001@gmail.com>
2024-09-20 21:34:06 +0500

    On First time messaging loads the DMContact and for Channels after checking the existing list

--- client/src/context/SocketContext.jsx
+++ client/src/context/SocketContext.jsx
@@ -1,5 +1,6 @@
+import { apiClient } from "@/lib/api-client";
 import { useAppStore } from "@/store";
-import { HOST } from "@/utils/constants";
+import { GET_CONTACTS_DM_ROUTE, GET_USER_CHANNELS_ROUTE, HOST } from "@/utils/constants";
 import { createContext, useContext, useRef, useEffect, useState } from "react";
 import { io } from "socket.io-client";
 
@@ -40,7 +41,11 @@ export const SocketProvider = ({ children }) => {
             });
 
             const handleReceiveMessage = (message) => {
-                const { selectedChatType, selectedChatData, addMessage, addContactInDMContacts } = useAppStore.getState();
+                const { selectedChatType, selectedChatData, addMessage,
+                    addContactInDMContacts,
+                    directMessagesContacts,
+                    setDirectMessagesContacts,
+                } = useAppStore.getState();
 
                 if (
                     selectedChatType &&
@@ -52,14 +57,48 @@ export const SocketProvider = ({ children }) => {
                     console.log("Received message:", message);
                     addMessage(message);
                     addContactInDMContacts(message)
+
+                } else if (!directMessagesContacts.some((contact) => contact._id === message.recipient._id)) {
+
+                    const getDMContacts = async () => {
+                        try {
+                            const response = await apiClient.get(GET_CONTACTS_DM_ROUTE, { withCredentials: true });
+                            if (response.status === 200 && response.data.contacts) {
+                                setDirectMessagesContacts(response.data.contacts);
+                            }
+                        } catch (error) {
+                            console.error("Error fetching contacts : ", error);
+                        }
+                    }
+                    getDMContacts();
+                    
                 }
             };
 
             const handleReceiveChannelMessage = (message) => {
-                const { selectedChatType, selectedChatData, addMessage, addChannelInChannelList } = useAppStore.getState();
+                const { selectedChatType, selectedChatData, addMessage, addChannelInChannelList,
+                    channels,
+                    setChannels,
+                } = useAppStore.getState();
+
                 if (selectedChatType && selectedChatData._id === message.channelId) {
                     addMessage(message);
                     addChannelInChannelList(message);
+                } else if (!channels.some((channel) => channel._id === message.channelId)) {
+                    
+                    const getChannels = async () => {
+                        try {
+                            const response = await apiClient.get(GET_USER_CHANNELS_ROUTE, { withCredentials: true });
+                            if (response.status === 200 && response.data.channels) {
+                                setChannels(response.data.channels);
+                            }
+                        } catch (error) {
+                            console.error("Error fetching channels:", error);
+                        }
+                    };
+
+                    getChannels();
+
                 }
             }
 
@@ -67,7 +106,7 @@ export const SocketProvider = ({ children }) => {
                 const { setDirectMessagesContactsLiveStatus } = useAppStore.getState();
                 setDirectMessagesContactsLiveStatus(message)
             }
-            
+
             const handleDisconnectContact = (message) => {
                 const { removeContactFromLiveContactList } = useAppStore.getState();
                 removeContactFromLiveContactList(message)
